b59b779f09326164cddbcc3d0811acd98dadfa88
Pramod Surve <Pramod.Surve@paccar.com>
2021-09-03 14:04:05 -0500

    Added Python Script

---
+++ Test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  3 13:53:53 2021
+
+@author: Pramod.Surve
+"""
+
+import pandas as pd
+
+cars = {'Brand': ['Civic', 'Corolla'],
+      'Color': ['Blue', 'Grey']
+      }
+
+df = pd.DataFrame(cars, columns= ['Brand', 'Color'])
+
+print (df)
+
+df.to_csv("test.csv")
